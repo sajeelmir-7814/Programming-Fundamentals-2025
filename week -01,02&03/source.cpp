815f87b7d9490b7421da5293cb1a0af2a23a7312
Sajeel MiR <sajeelmir7814@gmail.com>
2026-01-24 10:17:53 +0500

    PF-2025

---
+++ week -01,02&03/source.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	cout<<"\t Task-01\n";
+	cout<<"Welcome to the C++ Lab!\n" <<"You will learn basic output today\n" <<"Let's learn with the simple messages.\n";
+	
+	cout<<"\t Task-02\n";
+	cout<<"*\n" <<"* *\n" <<"* * *\n" <<"* *  * *\n" <<"* * * * * \n"; 
+	
+	cout<<"\t Task-03\n";
+	cout<<"Item\t\t Price\t\t Quality\t\t \n" <<"Pen\t\t $1\t\t 10\t\t \n" <<"Eraser\t\t $0.5\t\t 20\t\t \n" <<"Pencil\t\t $2\t\t 5\t\t \n";
+	
+	cout<<"\t Task-04\n";
+	cout<<"She said ,\"Let's learn C++!\" \n" <<"To start save your file in C:\\C++_Projects\\Lab1 \n" <<"Good Luck! \n";
+	
+	return 0;
+} 
