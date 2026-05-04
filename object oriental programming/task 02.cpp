bc302a6740d5c807d1fa39e0d29bcc7c7929c20c
Sajeel MiR <sajeelmir7814@gmail.com>
2026-05-04 07:09:13 +0100

    Update task 02.cpp

--- object oriental programming/task 02.cpp
+++ object oriental programming/task 02.cpp
@@ -7,13 +7,13 @@ private:
     double balance;
 
 public:
-    // Constructor
+
     Account(string a, double b) {
         accountNumber = a;
         balance = b;
     }
 
-    // Display function
+
     void display() {
         cout << "Account Number: " << accountNumber << endl;
         cout << "Balance: " << balance << endl;
@@ -24,14 +24,13 @@ int main() {
     string accNo;
     double bal;
 
-    // User input
+    
     cout << "Enter Account Number: ";
     cin >> accNo;
 
     cout << "Enter Balance: ";
     cin >> bal;
 
-    // Object create
     Account acc1(accNo, bal);
 
     cout << "\nAccount Details:\n";
